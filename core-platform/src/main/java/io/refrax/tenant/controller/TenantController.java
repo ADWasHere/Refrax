3828fb965802a6ff516ac2936665db2c57d86e93
Adrian Dietrich <adrian.dietrich8153@gmail.com>
2026-09-08 20:01:27 +0200

    Fix security error resposne to caller

--- core-platform/src/main/java/io/refrax/tenant/controller/TenantController.java
+++ core-platform/src/main/java/io/refrax/tenant/controller/TenantController.java
@@ -3,15 +3,17 @@ package io.refrax.tenant.controller;
 import io.refrax.tenant.TenantProvisioningService;
 import jakarta.inject.Inject;
 import jakarta.validation.Valid;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+import org.jboss.logging.Logger;
 
 @Path("/v1/tenants")
 public class TenantController {
 
+    private static final Logger LOG = Logger.getLogger(TenantController.class);
     @Inject
     TenantProvisioningService provisioningService;
 
@@ -22,7 +24,10 @@ public class TenantController {
             provisioningService.provisionTenant(req.schema());
             return Response.status(Response.Status.CREATED).build();
         } catch (Exception e) {
-            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(e.getMessage()).build();
+            LOG.error("Error occurred while creating tenant", e);
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
+                    .entity("An unexpected error occurred.")
+                    .build();
         }
     }
 }
